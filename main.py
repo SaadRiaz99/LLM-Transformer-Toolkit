3a2a9b3b6ef62488b0e2249844b2566f2e5b4ba6
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-03-17 09:24:07 +0500

    Add End at string and add Data about Pakistan

--- main.py
+++ main.py
@@ -15,3 +15,19 @@ print("Torch version:", torch.__version__)
 print("CUDA available:", torch.cuda.is_available())
 print("GPU name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "None")
 
+data = [
+    "Pakistan is a beautiful country with a rich history and culture.",
+    "he people of Pakistan are very kind and hospitable.",
+    "Pakistan has stunning mountains, rivers, and valleys.",
+    "The country has a strong and brave army.",
+    "Pakistani youth are talented and hardworking.",
+    "Pakistan has unique traditions and cultural diversity.",
+    "The land of Pakistan is fertile and good for agriculture.",
+    "Many languages and cultures live together in harmony in Pakistan.",
+    "Pakistan has shown courage and strength in difficult times.",
+    "We are proud of Pakistan and hope for its bright future. 🇵🇰"
+]
+
+datas = [s + "<End>" for s in data ]
+text = "".join(datas)
+print(text)
